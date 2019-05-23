209b2c79fd3269b82e755768e181fe3761256f75
Zoltan Egri <egri.zoltan.m@gmail.com>
2019-05-23 13:41:51 +0200

    demo code

--- Winsock_Server/main.c
+++ Winsock_Server/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
-char* error_to_string(int error_code);
+#define SERVER_PORT "27015"
+#define BUFF_LEN 1024
 
+void program_exit();
 
 int main()
 {
@@ -13,31 +16,100 @@ int main()
     WSADATA wsa_data;
 
     int error = WSAStartup(wversion_req, &wsa_data);
-    if(error) {
-        printf("WSAStartup failed with error code %d:\n\"%s\"\n", error, error_to_string(error));
+    if (error) {
+        printf("WSAStartup failed with error code '%d'\n", error);
+        WSACleanup();
+        return 1;
     } else {
-        printf("WSAStartup called successfully!\n");
+        printf("WinSock initialized successfully with version %d.%d\n",
+               LOBYTE(wsa_data.wVersion),
+               HIBYTE(wsa_data.wVersion));
     }
 
-    printf("Version: %d.%d", LOBYTE(wsa_data.wVersion), HIBYTE(wsa_data.wVersion));
+    ADDRINFOA hints;
+    ADDRINFOA* result = NULL;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    error = getaddrinfo(NULL, SERVER_PORT, &hints, &result);
+    if (error) {
+        printf("Address info resolving failed. Error: %d\n", error);
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET server_socket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (server_socket == INVALID_SOCKET) {
+        printf("Socket creation failed. Error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    error = bind(server_socket, result->ai_addr, (int) result->ai_addrlen);
+    freeaddrinfo(result);
+    if (error == SOCKET_ERROR) {
+        printf("Socket binding failed. Error: %d\n", WSAGetLastError());
+        closesocket(server_socket);
+        WSACleanup();
+        return 1;
+    }
+    if (listen(server_socket, SOMAXCONN) == SOCKET_ERROR) {
+        printf("Listen failed with error: %d\n", WSAGetLastError());
+        closesocket(server_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET client_socket;
+
+    client_socket = accept(server_socket, NULL, NULL);
+    if (client_socket == INVALID_SOCKET) {
+        printf("Client socket rejected. Error: %d\n", WSAGetLastError());
+        closesocket(server_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    char recieve_buffer[BUFF_LEN];
+    int recieved_len = 0;
+    int byte_rec = 0;
+    do {
+        byte_rec = recv(client_socket, recieve_buffer, recieved_len, 0);
+        if (byte_rec > 0) {
+            printf("Bytes recieved: %d\n", byte_rec);
+
+            int send_result = send(client_socket, recieve_buffer, byte_rec, 0);
+            if (send_result == SOCKET_ERROR) {
+                printf("Send failed. Error: %d\n", WSAGetLastError());
+                closesocket(client_socket);
+                WSACleanup();
+                return 1;
+            }
+        } else if (byte_rec == 0) {
+            printf("Connection closing...\n");
+        } else {
+            printf("Recieve failed. Error: %d\n", WSAGetLastError());
+            closesocket(client_socket);
+            WSACleanup();
+            return 1;
+        }
+
+    } while (byte_rec > 0);
+
+    error = shutdown(client_socket, SD_SEND);
+    if(error == SOCKET_ERROR) {
+        printf("Shutdown failed. Error: %d\n", WSAGetLastError());
+        closesocket(client_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    closesocket(client_socket);
     WSACleanup();
     return 0;
-}
-
-char* error_to_string(int error_code)
-{
-    switch(error_code) {
-        case WSASYSNOTREADY:
-            return "The underlying network subsystem is not ready for network communication!";
-        case WSAVERNOTSUPPORTED:
-            return "The version of Windows Sockets support requested is not provided by this particular Windows Sockets implementation!";
-        case WSAEINPROGRESS:
-            return "A blocking Windows Sockets 1.1 operation is in progress!";
-        case WSAEPROCLIM:
-            return "A limit on the number of tasks supported by the Windows Sockets implementation has been reached!";
-        case WSAEFAULT:
-            return "The lpWSAData parameter is not a valid pointer!";
-        default:
-            return "Unknown error!";
-    }
 }
