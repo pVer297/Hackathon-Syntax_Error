06c6b3ac34aacf4f0f5bb4171c3f27fe977446cf
Daniel Molnar <molnard0412@gmail.com>
2019-05-23 20:10:52 +0200

    easier to use socket interface

--- client_tcp/plainte_socket2019.c
+++ client_tcp/plainte_socket2019.c
@@ -1,5 +1,38 @@
 #include "plainte_socket2019.h"
 
+int create_client()
+{
+    // Declare and initialize variables
+    // Initialize Winsock
+    if(!start_wsa()) {
+        return 0;
+    }
+    // Create a SOCKET for connecting to server
+    SOCKET connect_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(!create_socket(connect_socket)) {
+        return 0;
+    }
+    // The sockaddr_in structure specifies the address family,
+    // IP address, and port of the server to be connected to.
+    struct sockaddr_in client_service;
+    client_service.sin_family = AF_INET;
+    client_service.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+    client_service.sin_port = htons(PORT);
+    // Connect to server
+    if(!connect_sck(connect_socket, client_service)) {
+        return 0;
+    }
+    // Send and receive data
+    if(!send_and_receive_data(connect_socket)) {
+        return 0;
+    }
+    // Close down everything
+    if(!close_sck(connect_socket)) {
+        return 0;
+    }
+    WSACleanup();
+}
+
 int start_wsa()
 {
     // Declare and initialize variables
@@ -101,7 +134,7 @@ int exit_command(int* username, char* user_input, char* usr_name)
         if (stricmp(c, "y") == 0 || stricmp(c, "yes") == 0) {
             return 0;
         } else if (stricmp(c, "n") == 0 || stricmp(c, "no") == 0) {
-            int a = rand() % 3;
+            int a = rand() % 4;
             switch (a) {
                 case 0:
                     printf("Tip: you can always be happy! :)\n");
@@ -111,6 +144,9 @@ int exit_command(int* username, char* user_input, char* usr_name)
                     break;
                 case 2:
                     printf("Error 404: Joke Not Found\n");
+                    break;
+                case 3:
+                    printf("Exam question about TCP/IP? just include plainte_socket2019!\n");
                     break;
                 default:
                     printf("Activating Witch Time...\n");
